a825b6db65af4632a1042b62715c1d5ba6695aa8
Suellen <isuelima@gmail.com>
2021-11-21 13:54:09 -0300

    status bar and background

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
-const App: React.FC = () => <View/>;
+const App: React.FC = () => (
+    <>
+        <StatusBar barStyle="light-content" backgroundColor="#E76F51" />
+        <View style={{ flex: 1, backgroundColor: '#E76F51' }} />
+    </>
+);
 
 export default App;
